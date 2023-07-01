cb4aa51257a801053bbcb185d30d3d62b2197115
Sahil <sahil@sahils-mbp.lan>
2023-07-01 14:56:57 -0700

    updates done by...

---
+++ app.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from flask import Flask, redirect, render_template, request, session, url_for, flash
+from pyMongo import MongoClient
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "erewewewrwe"
+app.config["MONGODB_URI"] = " "
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+
+    return wrap
+
+@app.route('/')
+@login_required
+def index():
+    return("home.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+
